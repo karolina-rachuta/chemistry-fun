beddda82f3f4efee7a1f135cd42f4bdb448fa45e
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-12-12 19:38:44 +0400

    added logic for weight conversion

--- client/src/components/calculators/MetricCalculator.jsx
+++ client/src/components/calculators/MetricCalculator.jsx
@@ -5,17 +5,28 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function MetricCalculator() {
     const [lengthInput, setLengthInput] = useState(0);
-    const [result, setResult] = useState('');
+    const [lengthResult, setLengthResult] = useState('');
+    const [weightInput, setWeightInput] = useState(0);
+    const [weightResult, setWeightResult] = useState('');
 
-    function handleUnit(e) {
+    function handleLengthUnits(e) {
         if (e.target.value === 'inch') {
-            setResult(`${lengthInput * 2.54} cm`);
+            setLengthResult(`${lengthInput * 2.54} cm`);
         } else if (e.target.value === 'foot') {
-            setResult(`${lengthInput * 30.48} cm`);
+            setLengthResult(`${lengthInput * 30.48} cm`);
         } else if (e.target.value === 'yard') {
-            setResult(`${lengthInput * 91.44} m`);
+            setLengthResult(`${lengthInput * 91.44} m`);
         } else if (e.target.value === 'mile') {
-            setResult(`${lengthInput * 1.609} km`);
+            setLengthResult(`${lengthInput * 1.609} km`);
+        } else return;
+    }
+    function handleWeightUnits(e) {
+        if (e.target.value === 'ounce') {
+            setWeightResult(`${weightInput * 28.3495} g`);
+        } else if (e.target.value === 'pound') {
+            setWeightResult(`${weightInput * 0.4536} kg`);
+        } else if (e.target.value === 'ton') {
+            setWeightResult(`${weightInput * 907.184} kg`);
         } else return;
     }
     return (
@@ -31,28 +42,102 @@ function MetricCalculator() {
                             value={lengthInput}
                             onChange={(e) => setLengthInput(e.target.value)}
                         />
-                        <button onClick={(e) => handleUnit(e)} value={'inch'}>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'inch'}
+                        >
                             inch <FontAwesomeIcon icon={faArrowRight} /> cm
                         </button>
-                        <button onClick={(e) => handleUnit(e)} value={'foot'}>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'foot'}
+                        >
                             foot <FontAwesomeIcon icon={faArrowRight} /> cm
                         </button>
-                        <button onClick={(e) => handleUnit(e)} value={'yard'}>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'yard'}
+                        >
                             yard <FontAwesomeIcon icon={faArrowRight} /> m
                         </button>
-                        <button onClick={(e) => handleUnit(e)} value={'mile'}>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'mile'}
+                        >
                             mile <FontAwesomeIcon icon={faArrowRight} /> km
                         </button>
                     </div>
                     <div>
-                        <h3>Result: {result}</h3>
+                        <h3>Result: {lengthResult}</h3>
                     </div>
                 </div>
                 <div>
                     <h2>Weight Conversion:</h2>
+                    <div>
+                        <input
+                            type="number"
+                            value={weightInput}
+                            onChange={(e) => setWeightInput(e.target.value)}
+                        />
+                        <button
+                            onClick={(e) => handleWeightUnits(e)}
+                            value={'ounce'}
+                        >
+                            ounce <FontAwesomeIcon icon={faArrowRight} /> g
+                        </button>
+                        <button
+                            onClick={(e) => handleWeightUnits(e)}
+                            value={'pound'}
+                        >
+                            pound <FontAwesomeIcon icon={faArrowRight} /> kg
+                        </button>
+                        <button
+                            onClick={(e) => handleWeightUnits(e)}
+                            value={'ton'}
+                        >
+                            ton <FontAwesomeIcon icon={faArrowRight} /> kg
+                        </button>
+                    </div>
+                    <div>
+                        <h3>Result: {weightResult}</h3>
+                    </div>
                 </div>
                 <div>
                     <h2>Temperature Conversion:</h2>
+                    <div>
+                        <input
+                            type="number"
+                            value={lengthInput}
+                            onChange={(e) => setLengthInput(e.target.value)}
+                        />
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'inch'}
+                        >
+                            inch <FontAwesomeIcon icon={faArrowRight} /> cm
+                        </button>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'foot'}
+                        >
+                            foot <FontAwesomeIcon icon={faArrowRight} /> cm
+                        </button>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'yard'}
+                        >
+                            yard <FontAwesomeIcon icon={faArrowRight} /> m
+                        </button>
+                        <button
+                            onClick={(e) => handleLengthUnits(e)}
+                            value={'mile'}
+                        >
+                            mile <FontAwesomeIcon icon={faArrowRight} /> km
+                        </button>
+                    </div>
+                    <div>
+                        <h3>Result: {lengthResult}</h3>
+                    </div>
                 </div>
             </h1>
         </div>
