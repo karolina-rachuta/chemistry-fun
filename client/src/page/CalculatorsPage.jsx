71af51f36fa6ea6adf3c929c266f2cbca145575f
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-01-06 14:42:20 +0400

    adding favicon and animation on hero

--- client/src/page/CalculatorsPage.jsx
+++ client/src/page/CalculatorsPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import LanguageNavigation from '../components/allQuizzes/LanguageNavigation';
 import MetricCalculator from '../components/calculators/MetricCalculator';
 function CalculatorsPage() {
     return (
