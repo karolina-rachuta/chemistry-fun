59ea0886d7324f2352e4c24c5c0ecafd64ce5e56
Karolina Rachuta <karolina.rachuta@gmail.com>
2026-03-26 14:07:37 +0000

    restyling hero section

--- client/src/components/homepage/Hero.tsx
+++ client/src/components/homepage/Hero.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
-
+import Button from '../ui/Button';
 import './Hero.css';
+import { Link } from 'react-router-dom';
 
 function Hero() {
     const { t } = useTranslation();
@@ -8,10 +9,30 @@ function Hero() {
         <section
             className="hero"
         >
+        <div className='hero-feature'> {t('hero.feature')}</div>
             <h1 className="hdl__main">
             {t('hero.welcome')}
             </h1>
             <span className='hdl__main-blue'>{t('hero.hdl')}</span>
+            <p className='hero-desc'>{t('hero.desc')}</p>
+            <div className='hero-btn-box'>
+                <Link to='/quizzes'>
+                    <Button
+                        variant="primary"
+                        className='cta-btn'
+                    >
+                        {t('hero.cta1')}
+                    </Button>
+                </Link>
+                <Link to='/calculators'>
+                    <Button
+                        variant="nav"
+                        className='cta-btn'
+                    >
+                        {t('hero.cta2')}
+                    </Button>
+                </Link>
+            </div>
         </section>
     );
 }
